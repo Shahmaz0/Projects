42041de42056b5e6ddf648949cd4ff7309e828e8
Shahma Ansari <shahmaansari@Shahmas-MacBook-Air.local>
2024-04-24 09:07:37 +0530

    Added: Some code

---
+++ contact-react-page/src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,12 @@
+import styles from './ContactForm.module.css';
+
+function ContactForm() {
+
+    return (
+        <div className={`${styles.contact_form} container`}>
+
+        </div>
+    )
+}
+
+export default ContactForm;
