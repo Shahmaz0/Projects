667a731458a2e84ffbe7c64ab99468693a06a5a5
Shahma Ansari <shahmaansari@Shahmas-MacBook-Air.local>
2024-04-27 23:35:50 +0530

    Added: Some code

---
+++ contact-react-page/src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import { MdMessage } from "react-icons/md";
+import styles from "./Button.module.scss";
+const Button = () => {
+    return (
+        <Button className={styles.primary_btn}>
+            <MdMessage/>
+            VIA SUPPORT CHAT
+        </Button>
+    )
+}
+export default Button;
--- contact-react-page/src/components/ContactForm/ContactForm.jsx
+++ contact-react-page/src/components/ContactForm/ContactForm.jsx
@@ -1,11 +1,13 @@
 import styles from './ContactForm.module.css';
-
 function ContactForm() {
 
     return (
-        <div className={`${styles.contact_form} container`}>
+        <section className={`${styles.container} container`}>
+            <div className={styles.contact_form}></div>
+            <div className={styles.contact_image}>
 
-        </div>
+            </div>
+        </section>
     )
 }
 
