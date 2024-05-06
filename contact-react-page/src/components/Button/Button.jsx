17122f32dbb252a13f61f57819f4636b33864f6a
Shahma Ansari <shahmaansari@Shahmas-MacBook-Air.local>
2024-05-06 22:35:22 +0530

    Added: Some code

--- contact-react-page/src/components/Button/Button.jsx
+++ contact-react-page/src/components/Button/Button.jsx
@@ -2,15 +2,11 @@ import { MdMessage } from "react-icons/md";
 import styles from "./Button.module.scss";
 const Button = () => {
     return (
-        // <Button className={styles.primary_btn}>
-        //     <MdMessage/>
-        //     VIA SUPPORT CHAT
-        // </Button>
-
         <Button className={styles.primary_btn}>
             <MdMessage/>
             VIA SUPPORT CHAT
         </Button>
+
     )
 }
 export default Button;
